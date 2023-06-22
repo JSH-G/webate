2888d5b36ab26fb2ac4583d148ee8d37c7987372
GoriPathan <goripathan77@gmail.com>
2023-06-22 15:00:47 +0500

    this is new post

--- app/routers/admin_side.py
+++ app/routers/admin_side.py
@@ -45,3 +45,37 @@ def get_all_hotel_admin(db: Session = Depends(get_db)):
 
     return {'status': "true","message":"success","body":response}
 
+
+@router.get('/get_hotel_info_admin', status_code=status.HTTP_200_OK)
+def get_hotel_info_admin(hotel_id: str, db: Session = Depends(get_db)):
+    hotel = db.query(models.Hotel_Sign_up).filter(models.Hotel_Sign_up.id == hotel_id).first()
+
+
+    ratings = db.query(models.Rating).filter(models.Rating.hotel_id == hotel.id).all()
+    rating_total = sum(rating.rating for rating in ratings) if ratings else 0
+    rating_average = rating_total / len(ratings) if ratings else 0
+    data = []
+    respons = {
+            'id': hotel.id,
+            'hotel_name': hotel.name,
+            'email': hotel.email,
+            'gallery': data, 
+            'hotel_description': hotel.discription,
+            'longitude': hotel.longitude,
+            'latitude':hotel.latitude,
+            'phone_number': hotel.phone_number,
+            'hotel_pic': hotel.hotel_image_url,
+            'hotel_logo': hotel.logo_image_url,
+            'upgrade': hotel.is_pro,
+            'rating_total': rating_total,
+            'rating_average': round(rating_average, 2)
+        }
+    check_image = db.query(models.Hotel_Gallery_Image).filter(models.Hotel_Gallery_Image.hotel_id == hotel.id).all()
+    for image in check_image:
+            image_data = {
+                'image_id': image.id,
+                'image': image.image
+            }
+            data.append(image_data)
+
+    return {"status": True,"message":"Success","body":respons}
